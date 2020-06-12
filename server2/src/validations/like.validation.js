694a8544d4abe06ce7afe788e33c1a479595c681
inbentor <chajyn@naver.com>
2020-06-13 00:49:03 +0900

    Update like.validation.js

--- server2/src/validations/like.validation.js
+++ server2/src/validations/like.validation.js
@@ -4,14 +4,12 @@ const { objectId } = require('./custom.validation');
 const createLike = {
   body: Joi.object().keys({
     postId: Joi.custom(objectId).required(),
-    userId: Joi.custom(objectId).required(),
   }),
 };
 
 const cancelLike = {
   body: Joi.object().keys({
     postId: Joi.custom(objectId).required(),
-    userId: Joi.custom(objectId).required(),
   }),
 };
 
